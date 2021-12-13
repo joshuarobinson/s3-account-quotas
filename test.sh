92465d18b9e1bc237eda213c33724088c06b3e9d
joshuarobinson <3334099+joshuarobinson@users.noreply.github.com>
2021-12-13 15:34:13 -0600

    Update test.sh
    Add example env for sending warning emails through gmail smtp

--- test.sh
+++ test.sh
@@ -10,4 +10,7 @@ FB_MGMT_TOKEN=T-REPLACE
 ACCOUNT="default"
 QUOTA="1TB"
 
-docker run -it --rm -e PUREFB_URL=$FB_MGMT_VIP -e PUREFB_API=$FB_MGMT_TOKEN $IMGNAME --account $ACCOUNT --quota $REPLACE
+docker run -it --rm \
+  -e PUREFB_URL=$FB_MGMT_VIP -e PUREFB_API=$FB_MGMT_TOKEN \
+  -e SMTP_EMAIL="something@gmail.com" -e SMTP_PASSWORD="secret" \
+  $IMGNAME --account $ACCOUNT --quota $REPLACE
