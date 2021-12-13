956577cda6ae288134508033179429a5be90cfba
Joshua Robinson <joshua.robinson@gmail.com>
2021-12-13 20:41:50 +0000

    Change default behavior to WARN only
    Add flag "--enforce" if you want to enforce the quota by disabling write
    access.

--- s3-account-quota.py
+++ s3-account-quota.py
@@ -47,6 +47,7 @@ if not FB_MGMT or not TOKEN:
 parser = argparse.ArgumentParser()
 parser.add_argument("--account", default="default")
 parser.add_argument("--quota", default="0")
+parser.add_argument("--enforce", dest='enforce', action='store_true')
 
 args = parser.parse_args()
 
@@ -80,44 +81,45 @@ except rest.ApiException as e:
 if total_account_bytes > quota_bytes:
     print("WARN Quota exceeded for account {}".format(account_name))
 
-    # Collect list of policies, in case we need to replace 'full-access'
-    disabled_policies = ['pure:policy/full-access', 'pure:policy/object-write']
-    res = fb.object_store_access_policies.list_object_store_access_policies()
-    all_policies = [r.name for r in res.items if r.name not in disabled_policies]
+    if args.enforce:
+        # Collect list of policies, in case we need to replace 'full-access'
+        disabled_policies = ['pure:policy/full-access', 'pure:policy/object-write']
+        res = fb.object_store_access_policies.list_object_store_access_policies()
+        all_policies = [r.name for r in res.items if r.name not in disabled_policies]
 
-    # List of all users in this account.
-    res = fb.object_store_users.list_object_store_users(filter='name=\'' + account_name + '/*\'')
-    users = [r.name for r in res.items]
+        # List of all users in this account.
+        res = fb.object_store_users.list_object_store_users(filter='name=\'' + account_name + '/*\'')
+        users = [r.name for r in res.items]
 
-    recovery_commands = []
+        recovery_commands = []
 
-    # For each user, remove the object-write policy. Handle 'full-access' special case.
-    for user in users:
-        # Get existing policies for the user.
-        res = fb.object_store_access_policies.list_object_store_access_policies_object_store_users(member_names=[user])
-        user_policies = [r.policy.name for r in res.items]
+        # For each user, remove the object-write policy. Handle 'full-access' special case.
+        for user in users:
+            # Get existing policies for the user.
+            res = fb.object_store_access_policies.list_object_store_access_policies_object_store_users(member_names=[user])
+            user_policies = [r.policy.name for r in res.items]
 
-        if 'pure:policy/full-access' in user_policies:
-            # Replace full-access  with the list of constituent policies.
-            add_policies = [p for p in all_policies if p not in user_policies]
-            print("Downgrading user {} from full-access policy. Adding: {}".format(user, ",".join(add_policies)))
-            for policy in add_policies:
-                res = fb.object_store_access_policies.add_object_store_access_policies_object_store_users(member_names=[user], policy_names=[policy])
-                recovery_commands.append("purepolicy obj access remove --user {} {}".format(user, policy))
+            if 'pure:policy/full-access' in user_policies:
+                # Replace full-access  with the list of constituent policies.
+                add_policies = [p for p in all_policies if p not in user_policies]
+                print("Downgrading user {} from full-access policy. Adding: {}".format(user, ",".join(add_policies)))
+                for policy in add_policies:
+                    res = fb.object_store_access_policies.add_object_store_access_policies_object_store_users(member_names=[user], policy_names=[policy])
+                    recovery_commands.append("purepolicy obj access remove --user {} {}".format(user, policy))
 
-            # Then, remove full-access
-            res = fb.object_store_access_policies.remove_object_store_access_policies_object_store_users(
-                member_names=[user], policy_names=["pure:policy/full-access"])
-            recovery_commands.append("purepolicy obj access add --user {} {}".format(user, "pure:policy/full-access"))
+                # Then, remove full-access
+                res = fb.object_store_access_policies.remove_object_store_access_policies_object_store_users(
+                    member_names=[user], policy_names=["pure:policy/full-access"])
+                recovery_commands.append("purepolicy obj access add --user {} {}".format(user, "pure:policy/full-access"))
 
-        if 'pure:policy/object-write' in user_policies:
-            res = fb.object_store_access_policies.remove_object_store_access_policies_object_store_users(
-                member_names=[user], policy_names=["pure:policy/object-write"])
-            recovery_commands.append("purepolicy obj access add --user {} {}".format(user, "pure:policy/object-write"))
+            if 'pure:policy/object-write' in user_policies:
+                res = fb.object_store_access_policies.remove_object_store_access_policies_object_store_users(
+                    member_names=[user], policy_names=["pure:policy/object-write"])
+                recovery_commands.append("purepolicy obj access add --user {} {}".format(user, "pure:policy/object-write"))
 
-if recovery_commands:
-    print("To recover policies back to original state, issue the following CLI commands:")
-    for c in recovery_commands:
-        print(c)
+        if recovery_commands:
+            print("To recover policies back to original state, issue the following CLI commands:")
+            for c in recovery_commands:
+                print(c)
 
 fb.logout()
